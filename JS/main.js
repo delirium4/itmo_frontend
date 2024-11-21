ab2399fc671272717abdc5e6669fa2f2c1d68465
delirium4 <112827428+delirium4@users.noreply.github.com>
2024-11-21 15:23:51 +0300

    lab-4

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+(function () {
+    window.addEventListener('load', function() {
+        const loadTime = performance.now();
+        const footer = document.querySelector('footer');
+
+        if (footer) {
+            const stats = document.createElement('div');
+            stats.style.fontSize = '12px';
+            stats.style.color = 'gray';
+            stats.innerText = `Page Load Time: ${loadTime} ms`;
+            footer.appendChild(stats);
+        }
+
+        console.log(`Page loaded in ${loadTime} ms`);
+    });
+})();
